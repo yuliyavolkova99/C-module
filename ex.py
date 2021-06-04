15d1becd3df5dbad8dafb58eb37d4196bf1b8fc1
yuliyavolkova99 <ysdv99@gmail.com>
2021-06-04 03:28:58 +0300

    C-module

--- ex.py
+++ ex.py
@@ -4,6 +4,5 @@ sys.path.append('C:/Users/User/Desktop/XXX.com/')
 import sortP
 print(sortP.bubblesort([1,343,542,-56,0,23]))
 print(sortP.insertionsort([1,343,542,-56,0,23]))
-print(sortP.choicesort([1,343,542,-56,0,23]))
 print(sortP.shellsort([1,343,542,-56,0,23]))
 
